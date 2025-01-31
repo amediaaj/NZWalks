ccedc95d85dbcb304eada40ea03fd36d7347361a
Alex Amedia <aamedia@asu.edu>
2025-01-31 13:46:38 -0700

    Create New Controller (Regions Controller)

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:1234/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        // GET ALL REGIONS
+        // GET: https://localhost:1234/api/regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Aucklan Region",
+                    Code = "AKL",
+                    RegionImageUrl = "https://images.pixels.com/photos/AKL"
+                },
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Wellington Region",
+                    Code = "WGL",
+                    RegionImageUrl = "https://images.pixels.com/photos/AKL"
+                }
+            };
+
+            return Ok(regions);
+        }
+    }
+}
