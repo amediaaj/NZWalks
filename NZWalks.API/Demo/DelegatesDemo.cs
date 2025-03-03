9428e8269c2e59ea7ad445a66d37e1414f0d5bc9
Alex Amedia <aamedia@asu.edu>
2025-03-03 11:20:27 -0700

    DelegatesDemo

---
+++ NZWalks.API/Demo/DelegatesDemo.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+namespace NZWalks.API.Demo
+{
+    public class DelegatesDemo : IMyService
+    {
+        private readonly int _serviceId;
+
+        // Delegates define a method signature,
+        // and any method assigned to a delegate must match this signature.
+
+        // 1. Declaration:
+        public delegate void Notify(string message);
+
+        public void ExecuteDemo()
+        {
+            // 2. Instantiation:
+            Notify notifyDelegate = ShowMessage;
+
+            // Old way
+            // Notify notifyDelegate = new Notify(ShowMessage);
+
+            // 3. Invocation:
+            notifyDelegate("Hello, Delegates!");
+        }
+
+        public void LogCreation(string message)
+        {
+            Debug.WriteLine($"\n{message} - Service ID: {_serviceId}");
+        }
+
+        static void ShowMessage(string message)
+        {
+            Debug.WriteLine(message);
+        }
+    }
+}
--- NZWalks.API/Enums/EnumsC.cs
+++ NZWalks.API/Enums/EnumsC.cs
@@ -1,4 +1,5 @@
 ﻿namespace NZWalks.API
 {
     enum Day { Mo, Tu, We, Th, Fr, Sa, Su};
+    enum Month { Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec};
 }
